3729c517a2096b701ee0f7da8fa97532f7d65aab
Roman Iakobchuk <kaboom.ua@gmail.com>
2017-10-23 10:44:59 +0300

    add HW1

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import {Route} from 'react-router-dom'
+import {Route, NavLink} from 'react-router-dom'
 import ProtectedRoute from './common/ProtectedRoute'
 import AdminPage from './routes/Admin'
 import AuthPage from './routes/Auth'
+import PersonPage from './routes/PersonPage'
 
 class App extends Component {
     static propTypes = {
@@ -13,7 +14,12 @@ class App extends Component {
         return (
             <div>
                 <h1>Hello world</h1>
+                <ul>
+                    <li><NavLink to='/admin' activeStyle = {{color: 'red'}}>admin</NavLink></li>
+                    <li><NavLink to='/people' activeStyle = {{color: 'red'}}>people</NavLink></li>
+                </ul>
                 <ProtectedRoute path = '/admin' component = {AdminPage}/>
+                <ProtectedRoute path="/people" component={PersonPage}/>
                 <Route path = '/auth' component = {AuthPage}/>
             </div>
         )
--- src/components/common/ErrorField.js
+++ src/components/common/ErrorField.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class ErrorField extends Component {
-    static propTypes = {
-
-    };
-
-    render() {
-        const {input, meta: {error, touched}, type, label} = this.props
-        const errorText = touched && error && <div style = {{color: 'red'}}>{error}</div>
-        return (
-            <div>
-                {label} <input {...input} type = {type} />
-                {errorText}
-            </div>
-        )
-    }
+function ErrorField(props) {
+    const {input, type, meta: {error, touched}} = props
+    const errorText = touched && error && <div style={{color: 'red'}}>{error}</div>
+    return (
+        <div>
+            <label>{input.name}</label>
+            <input {...input} type={type}/>
+            {errorText}
+        </div>
+    )
 }
 
 export default ErrorField
